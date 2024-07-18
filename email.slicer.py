2307a41904b813d6d63e15e28cf7a2827fc297b0
kaspersyq <arstyqw@gmail.com>
2024-07-18 13:48:26 +0500

    email first

---
+++ email.slicer.py
@@ -0,0 +1,10 @@
+# email slicer
+
+email = input('Enter your email address: ').strip()
+username = email[:email.index('@')]
+domain_name = email[email.index('@') + 1:]
+
+if domain_name == 'gmail.com':
+    print(f"Hey {username}, I see your email is registered with Google. That's cool!.")
+else:
+    print(f"Hey {username}, looks like you've got your own custom setup at {domain_name}. Impressive!")
