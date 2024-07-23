08dbe359355f86fc58a0a69b5ac130dd5251adb6
kaspersyq <arstyqw@gmail.com>
2024-07-23 20:18:18 +0500

    added odd or even

---
+++ odd_or_even.py
@@ -0,0 +1,11 @@
+# odd or evengt
+
+answer = 'y'
+while answer.lower() == 'y':
+    print('What number are you thinking?')
+    number = int(input())
+    if number % 2 == 1:
+        print("That's an odd number! Have another? y = yes, n = no")
+    else:
+        print("That's an even number! Have another? y = yes, n = no")
+    answer = input()
