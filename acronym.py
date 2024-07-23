9636f3848688414b9fc92c0a20fb4a72f63a4c4f
kaspersyq <arstyqw@gmail.com>
2024-07-23 20:36:45 +0500

    added acronym

---
+++ acronym.py
@@ -0,0 +1,9 @@
+# What's my acronym?
+
+print('Please enter the full meaning of an organization or concept')
+meaning = input()
+words_list = meaning.split()
+acronym = ''
+for i in range(len(words_list)):
+    acronym += words_list[i][0].upper()
+print(acronym)
