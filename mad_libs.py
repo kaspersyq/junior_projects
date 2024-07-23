6bc801bbf9fbfe36f12c596b176e4b6eaa52db2a
kaspersyq <arstyqw@gmail.com>
2024-07-23 20:44:40 +0500

    added mad_libs

---
+++ mad_libs.py
@@ -0,0 +1,9 @@
+# Mad libs
+
+color = input('Enter a color: ')
+noun = input('Enter a noun: ')
+verb = input('Enter a verb: ')
+
+print(f'Roses are {color}')
+print(f'{noun} are blue')
+print(f'I {verb} you')
